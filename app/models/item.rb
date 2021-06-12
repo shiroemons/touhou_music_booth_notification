a71feea3c81ee1bcaf26ebba8fadcc12c3b7014b
shiroemons <shiroemons@gmail.com>
2021-06-12 12:04:56 +0900

    Add circle_name and album_name method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  def circle_name
+    name.match(/(?<circle>.*?) - (?<album>.*)/)[:circle]
+  end
+
+  def album_name
+    name.match(/(?<circle>.*?) - (?<album>.*)/)[:album]
+  end
 end
